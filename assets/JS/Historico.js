c8234a277c7d904253b26ad73ee2c19e9d70e23b
Bruno <brunoalburquerque909@gmail.com>
2024-09-05 15:47:24 -0300

    Update Historico.js

--- assets/JS/Historico.js
+++ assets/JS/Historico.js
@@ -1,41 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
   const historicoCampo = document.querySelector('.historico-campo');
-  console.log('Elemento historicoCampo encontrado:', historicoCampo);
 
   // Obtém o histórico armazenado no localStorage
   const historico = JSON.parse(localStorage.getItem('historico') || '[]');
-  console.log('Dados do localStorage:', historico);
 
   // Adiciona cada item ao histórico na página
-  if (historico.length > 0) {
-    historico.forEach(item => {
-      const itemHistorico = document.createElement('div');
-      itemHistorico.className = 'item-historico';
-      itemHistorico.innerHTML = `
-        <div class="item-resposta">
-          <div class='header'>
-            <h2 class="titulo">${item.titulo}</h2>
-            <button onclick="Deletar_pesquisa(this)" class="deletar"><i class="mdi mdi-delete"></i>Deletar</button>
-          </div>
-          <p class="description">
+  historico.forEach(item => {
+    const itemHistorico = document.createElement('div');
+    itemHistorico.className = 'item-historico';
+    itemHistorico.innerHTML = `
+      <div class="item-resposta">
+        <div class='header'>
+          <h2 class="titulo">${item.titulo}</h2>
+          <button onclick="Deletar_pesquisa(this)" class="deletar"><i class="mdi mdi-delete"></i>Deletar</button>
+        </div>
+        <p class="description">
           ${item.descricao}
           <a class="more-info" href="${item.link}" target="_blank" rel="noopener noreferrer">Saiba Mais</a>
-          </p>
-        </div>
-      `;
-      historicoCampo.appendChild(itemHistorico);
-    });
-  } else {
-    const mensagemVazio = document.createElement('p');
-    mensagemVazio.textContent = 'Nenhuma pesquisa foi realizada ainda.';
-    historicoCampo.appendChild(mensagemVazio);
-  }
+        </p>
+      </div>
+    `;
+    historicoCampo.appendChild(itemHistorico);
+  });
 });
 
 function Deletar_pesquisa(button) {
   const itemHistorico = button.closest('.item-historico');
   if (itemHistorico) {
-    const tituloParaDeletar = itemHistorico.querySelector('.titulo').textContent;
-    itemHistorico.remove(); // Remove o item do DOM
+    itemHistorico.remove(); // Remove o item do histórico
 
-    // Atualiza o localStorage removendo o
+    // Atualiza o localStorage
+    const tituloParaDeletar = itemHistorico.querySelector('.titulo').textContent;
+    let historico = JSON.parse(localStorage.getItem('historico') || '[]');
+    historico = historico.filter(item => item.titulo !== tituloParaDeletar);
+    localStorage.setItem('historico', JSON.stringify(historico));
+  }
+}
